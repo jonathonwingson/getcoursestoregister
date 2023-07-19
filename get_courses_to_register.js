cdb46c2a34d297e012876888130fa9e09e001d9e
jWing <78874755+jonathonwingson@users.noreply.github.com>
2023-07-19 16:53:37 -0400

    Update get_courses_to_register.js
    working

--- get_courses_to_register.js
+++ get_courses_to_register.js
@@ -1,8 +1,4 @@
 
-const studentCourses = ['COMP2606', 'COMP1601', 'FOUN1105'];
-const programmeCourses = ['COMP1601', 'FOUN1105', 'COMP2606', 'INFO3609', 'COMP3610'];
-const semesterCourses = ['COMP3610', 'COMP1601', 'FOUN1105', 'COMP2606', 'COMP1602', 'INFO3609'];
-
 // const studentCourses = [
 //   { id: 1, grade: "A-", created_at: "2023-07-12 17:36:01.905+00", updated_at: "2023-07-12 17:36:01.905+00", studentId: "816026602", courseCode: "COMP2606" },
 //   { id: 2, grade: "A", created_at: "2023-07-12 17:36:02.006+00", updated_at: "2023-07-12 17:36:02.006+00", studentId: "816026602", courseCode: "COMP1601" },
@@ -26,6 +22,11 @@ const semesterCourses = ['COMP3610', 'COMP1601', 'FOUN1105', 'COMP2606', 'COMP16
 //   {id: 1, created_at: "2023-07-11 14:19:22.847+00", updated_at: "2023-07-11 14:19:22.847+00", semesterId: 1, courseCode: "COMP1602"},
 // ]
 
+const programmeId = 1;
+const studentCourses = ['COMP2606', 'COMP1601', 'FOUN1105'];
+const programmeCourses = ['COMP1601', 'FOUN1105', 'COMP2606', 'INFO3609', 'COMP3610'];
+const semesterCourses = ['COMP3610', 'COMP1601', 'FOUN1105', 'COMP2606', 'COMP1602', 'INFO3609'];
+
 const prerequisites = [
   { courseCode: "INFO3610", groupId: 2, programmeId: 1 },
   { courseCode: "INFO3610", groupId: 4, programmeId: 1 },
@@ -39,6 +40,11 @@ const courseGroups = [
   { groupId: 3, courseCode: "COMP2603" },
   { groupId: 3, courseCode: "COMP2606" },
   { groupId: 4, courseCode: "INFO2600" },
+];
+
+const antirequisites = [
+  {id: 1, created_at: "2023-07-11 14:19:30.016+00", updated_at: "2023-07-11 14:19:30.016+00", courseCode: "INFO3609", antirequisiteCourseCode: "INFO3610"},
+  {id: 2, created_at: "2023-07-11 14:19:30.016+00", updated_at: "2023-07-11 14:19:30.016+00", courseCode: "INFO3610", antirequisiteCourseCode: "INFO3609"},
 ];
 
 // returns true if all courses in the group is in the student courses
@@ -74,6 +80,8 @@ function atLeastOneGroupSatisfied(groupIds) {
 // checks if the prerequisites a course in a programme is satisfied  
 function arePrerequisitesSatisfied(courseCode, programmeId) {
 
+  let groupIds = [];
+
   for (j = 0; j < prerequisites.length; j++) {
     // if course has prereq for programme
     if (prerequisites[j].courseCode === courseCode && prerequisites[j].programmeId === programmeId) {
@@ -83,13 +91,22 @@ function arePrerequisitesSatisfied(courseCode, programmeId) {
   }
 
   // if there are no groups return true OR if at least one group is satisfied return true
-  return groupIds.length ===0 || atLeastOneGroupSatisfied(groupIds);
+  return groupIds.length === 0 || atLeastOneGroupSatisfied(groupIds);
   
 }
 
 // checks if the student did any anti-requisites for the course
 function completedAntirequisites(courseCode){
-
+  for( let i=0; i<antirequisites.length; i++){
+    if( antirequisites[i].courseCode === courseCode){
+      for( let j=0; j<studentCourses.length; j++){
+        if(studentCourses[j].courseCode === antirequisites[i].antirequisiteCourseCode){
+          return true;
+        }
+      }
+    }
+  }
+  return false;
 }
 
 // returns a list of courses the student can register for in the next semester
@@ -101,6 +118,7 @@ function get_courses_to_register(programmeId, studentCourses, programmeCourses, 
   for (i = 0; i < programmeCourses.length; i++) {
 
     let prereqSatisfied = false;
+    let completedAntireq = false;
 
     // if programmeCourse not completed by the student and is available in the semester
     if (!studentCourses.includes(programmeCourses[i]) && semesterCourses.includes(programmeCourses[i])) {
@@ -110,12 +128,21 @@ function get_courses_to_register(programmeId, studentCourses, programmeCourses, 
 
       // check if the student has done any anti-requisites
       completedAntireq = completedAntirequisites(programmeCourses[i].courseCode);
+    
     }
 
-    return registerableCourses;
+    // if student satisfies the prereqs and have not done any anti-reqs, course is registerable 
+    if( prereqSatisfied === true && completedAntireq === false){
+      registerableCourses.push(programmeCourses[i]);
+      // console.log("push course: ", programmeCourses[i]);
+    }
+
+    
   }
+
+  return registerableCourses;
 }
 
 
 const remainingCourses = get_courses_to_register(programmeId, studentCourses, programmeCourses, semesterCourses, prerequisites, courseGroups);
-console.log('Remaining courses:', remainingCourses);
+console.log('Registerable courses:', remainingCourses);
